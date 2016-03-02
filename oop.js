e5d9e6bd201dbec95463ba008c62553aa7b8c350
Marcel <m.akerboom@rocleiden.nl>
2016-03-02 22:15:30 +0100

    Extracted prototypes.

--- oop.js
+++ oop.js
@@ -1,3 +1,5 @@
+// we laten het initieren van de applicatie over aan een functie.
+// deze functie geeft de applicatie terug, voor bv debugging.
 function init() {
     return new Application();
 }
@@ -14,53 +16,51 @@ function Application() {
 function StartMenu() {
     this.element = document.getElementById('start-menu');
     var shutDownMenu = this.shutDownMenu = new ShutDownMenu();
+    this.close();
 
     document.getElementById('options-button').addEventListener('click', function() {
         shutDownMenu.toggle();
     });
-
-    this.open = function() {
-        this.element.style.display = 'block';
-        this.opened = true;
-    };
-
-    this.close = function() {
-        this.element.style.display = 'none';
-        this.shutDownMenu.close();
-        this.opened = false;
-    };
-
-    this.toggle = function() {
-        if(this.opened) {
-            this.close();
-        } else {
-            this.open();
-        }
-    };
-
-    this.close();
 }
+
+StartMenu.prototype.open = function() {
+    this.element.style.display = 'block';
+    this.opened = true;
+};
+
+StartMenu.prototype.close = function() {
+    this.element.style.display = 'none';
+    this.shutDownMenu.close();
+    this.opened = false;
+};
+
+StartMenu.prototype.toggle = function() {
+    if(this.opened) {
+        this.close();
+    } else {
+        this.open();
+    }
+};
 
 function ShutDownMenu() {
     this.element = document.getElementById('shutdown-menu');
-
-    this.open = function() {
-        this.element.style.display = '';
-        this.opened = true;
-    };
-
-    this.close = function() {
-        this.element.style.display = 'none';
-        this.opened = false;
-    };
-
-    this.toggle = function() {
-        if(this.opened) {
-            this.close();
-        } else {
-            this.open();
-        }
-    };
-
     this.close();
 }
+
+ShutDownMenu.prototype.open = function() {
+    this.element.style.display = '';
+    this.opened = true;
+};
+
+ShutDownMenu.prototype.close = function() {
+    this.element.style.display = 'none';
+    this.opened = false;
+};
+
+ShutDownMenu.prototype.toggle = function() {
+    if(this.opened) {
+        this.close();
+    } else {
+        this.open();
+    }
+};
